09cb816f0e20c8f46b81d9800a30ad6ec79f2e1f
tiagoventurini <engenheirotiagoventurini@gmail.com>
2021-01-08 20:41:49 -0300

    Fizemos metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -48,6 +48,43 @@ class Usuario {
         }
 
     }
+    ///// Para montar uma lista de usuarios /////////////////////
+
+    public static function getList(){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuario ORDER BY deslogin;");
+
+    }
+
+    public static function search($login){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuario WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%".$login."%"));
+    }
+
+    public function login($login, $password){
+  
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuario WHERE deslogin = :LOGIN AND dessenha = :PASSAWORD", 
+        array(":LOGIN"=>$login,":PASSWORD"=>$password));
+
+        if(count($results) > 0){
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        } else {
+
+            throw new Exception("Login e/ou senha ivalidos.");
+        }
+    }
 
     public function __toString(){
         return json_encode(array(
--- index.php
+++ index.php
@@ -2,10 +2,25 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(4);
+//echo $root;
 
-$root->loadbyId(4);
+// Carrega uma lista de usuario
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+// Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("Jo");
+
+//echo json_encode($search);
+
+// carrega um usuario usando o login e a senha.
+
+$usuario = new Usuario();
+$usuario->login('Josue','999');
+
+echo $usuario;
 
 ?>
